076598e601c3ceb04d90842d9c840bbe46e71070
venkat-abhi <venkat.abhishek.1999@gmail.com>
2019-12-10 20:49:45 +0530

    fix typos and move to header file

---
+++ stack/stack_stlListBased.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <list>
+
+template <typename T>
+class Stack
+{
+private:
+	std::list<T> l;
+
+public:
+	Stack(){}
+
+	Stack(T first_elem)
+	{
+		Stack<T>::push(first_elem);
+	}
+
+	T pop();
+	T peek();
+
+	int size();
+
+	bool isEmpty();
+
+	void push(T);
+};
+
+template <typename T>
+int Stack<T>::size()
+{
+	return l.size();
+}
+
+template <typename T>
+bool Stack<T>::isEmpty()
+{
+	return l.empty();
+}
+
+template <typename T>
+void Stack<T>::push(T ele)
+{
+	l.push_back(ele);
+}
+
+template <typename T>
+T Stack<T>::pop()
+{
+	if (Stack<T>::isEmpty()) {
+		throw std::underflow_error("Stack Empty!");
+	}
+
+	T top_ele = l.back();
+	l.pop_back();
+	return top_ele;
+}
+
+template <typename T>
+T Stack<T>::peek()
+{
+	if (Stack<T>::isEmpty()) {
+		throw std::underflow_error("Stack Empty!");
+	}
+
+	return l.back();
+}
